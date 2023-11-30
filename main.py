ce936e0ac69dc9d2218a9f912f34f81188afa38d
wookiua <maolegtor@gmail.com>
2023-11-30 18:58:21 +0100

    add_output_save

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ def all_points(dataset):
     y_values = [point[0] for point in dataset]
 
     plt.figure(figsize=(960/80, 540/80))
+
     plt.scatter(x_values, y_values, color='blue', marker='o')
+
+    plt.savefig('output_plot.png')
+    
     plt.show()
 
 file_DS5 = 'DS5.txt'
