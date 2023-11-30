f33b6e66569ebf93a9066c877fb8b031771b4ac4
wookiua <maolegtor@gmail.com>
2023-11-30 18:37:24 +0100

    lab2

---
+++ main.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+def read_dataset(file_DS5):
+    with open(file_DS5, 'r') as file:
+        pairs = file.readlines()
+        dataset = [list(map(int, line.strip().split())) for line in pairs]
+    return dataset
+
+def all_points(dataset):
+    x_values = [point[1] for point in dataset]
+    y_values = [point[0] for point in dataset]
+
+    plt.figure(figsize=(960/80, 540/80))
+    plt.scatter(x_values, y_values, color='blue', marker='o')
+    plt.show()
+
+file_DS5 = 'DS5.txt'
+dataset = read_dataset(file_DS5)
+all_points(dataset)
